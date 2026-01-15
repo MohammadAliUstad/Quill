5538475482ecf2e8df7620b863fe3a54170830cd
Mohammad Ali <mohammadaliustad@gmail.com>
2026-01-15 14:15:00 +0530

    Clean-up

--- app/src/main/java/com/yugentech/quill/ui/dash/models/SoundOption.kt
+++
@@ -1,9 +0,0 @@
-package com.yugentech.quill.ui.dash.models
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class SoundOption(
-    val label: String,
-    val id: String,
-    val icon: ImageVector
-)
