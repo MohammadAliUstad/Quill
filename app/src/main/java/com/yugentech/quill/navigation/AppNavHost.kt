9979e319f1d7f63f3ba3afe3287e515b2aa7b297
Mohammad Ali <mohammadaliustad@gmail.com>
2026-02-08 10:30:00 +0530

    Add new feature implementation

--- app/src/main/java/com/yugentech/quill/navigation/AppNavHost.kt
+++ app/src/main/java/com/yugentech/quill/navigation/AppNavHost.kt
@@ -9,13 +9,14 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.yugentech.quill.network.remote.Book
+import com.yugentech.quill.bookDetails.BookDetailsViewModel
+import com.yugentech.quill.library.LibraryViewModel
 import com.yugentech.quill.theme.ThemeViewModel
 import com.yugentech.quill.ui.config.screens.AboutScreen
 import com.yugentech.quill.ui.config.screens.AppearanceScreen
 import com.yugentech.quill.ui.config.screens.AttributionsScreen
-import com.yugentech.quill.ui.dash.screens.AiraChatScreen
-import com.yugentech.quill.ui.dash.screens.mainScreen.MainScreen
+import com.yugentech.quill.ui.dash.screens.airaScreen.AiraChatScreen
+import com.yugentech.quill.ui.dash.screens.mainScreen.parent.MainScreen
 import com.yugentech.quill.ui.dash.utils.defaultEnterTransition
 import com.yugentech.quill.ui.dash.utils.defaultExitTransition
 import com.yugentech.quill.ui.dash.utils.defaultPopEnterTransition
@@ -23,11 +24,10 @@ import com.yugentech.quill.ui.dash.utils.defaultPopExitTransition
 import com.yugentech.quill.ui.dash.screens.readerScreen.parent.ReaderScreen
 import com.yugentech.quill.ui.dash.screens.bookDetailsScreen.parent.BookDetailsScreen
 import com.yugentech.quill.ui.dash.screens.ManageCategoriesScreen
-import com.yugentech.quill.ui.dash.screens.StandardScreen
+import com.yugentech.quill.ui.dash.screens.standardScreen.parent.StandardScreen
 import com.yugentech.quill.viewModels.StandardViewModel
 import kotlinx.serialization.json.Json
 import org.koin.androidx.compose.koinViewModel
-import java.net.URLDecoder
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
@@ -51,18 +51,15 @@ fun AppNavHost(
         }
 
         composable(Screens.Main.route) {
+            val libraryViewModel: LibraryViewModel = koinViewModel()
+
             MainScreen(
                 onLibraryBookClick = { book ->
                     val bookJson = Json.encodeToString(book)
                     val encodedJson = URLEncoder.encode(bookJson, StandardCharsets.UTF_8.toString())
                     navController.navigate("${Screens.BookDetailsScreen.route}/$encodedJson")
                 },
-                onDiscoverItemClick = { book ->
-                    val bookJson = Json.encodeToString(book)
-                    val encodedJson =
-                        URLEncoder.encode(bookJson, StandardCharsets.UTF_8.toString())
-                    navController.navigate("${Screens.BookDetailsScreen.route}/$encodedJson")
-                },
+                onDiscoverItemClick = { },
                 onSourceClick = { sourceId ->
                     if (sourceId == "standard_ebooks") {
                         navController.navigate(Screens.StandardEbooks.route)
@@ -79,7 +76,8 @@ fun AppNavHost(
                 },
                 onFABClick = {
                     navController.navigate(Screens.Aira.route)
-                }
+                },
+                libraryViewModel = libraryViewModel
             )
         }
 
@@ -115,40 +113,51 @@ fun AppNavHost(
         // ... inside AppNavHost content
 
         composable(
-            route = "${Screens.BookDetailsScreen.route}/{bookJson}",
-            arguments = listOf(navArgument("bookJson") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val bookJson = backStackEntry.arguments?.getString("bookJson")
-            if (bookJson != null) {
-                // Decode JSON back to Book object
-                val decodedJson = URLDecoder.decode(bookJson, StandardCharsets.UTF_8.toString())
-                val book = Json.decodeFromString<Book>(decodedJson)
+            route = "${Screens.BookDetailsScreen.route}/{book_json}",
+            arguments = listOf(
+                navArgument("book_json") { type = NavType.StringType }
+            )
+        ) {
+            val bookDetailsViewModel: BookDetailsViewModel = koinViewModel()
 
-                BookDetailsScreen(
-                    book = book,
-                    onBackClick = { navController.popBackStack() },
-                    onReadClick = { bookId ->
-                        // Navigate to the ReaderScreen using the ID passed from BookDetails
-                        navController.navigate(Screens.Reader.createRoute(bookId))
-                    },
-                    onManageCategoriesClick = {
-                        // Navigate to Manage Categories if needed
-                        navController.navigate(Screens.ManageCategories.route)
+            BookDetailsScreen(
+                onBackClick = { navController.popBackStack() },
+                // Inside AppNavHost composable for BookDetailsScreen
+                onReadClick = { bookId, chapterHref ->
+                    // FIX: Always encode the bookId before passing it in a route
+                    val encodedBookId = URLEncoder.encode(bookId, StandardCharsets.UTF_8.toString())
+
+                    if (chapterHref != null) {
+                        val encodedHref = URLEncoder.encode(chapterHref, StandardCharsets.UTF_8.toString())
+                        navController.navigate("reader/$encodedBookId?href=$encodedHref")
+                    } else {
+                        navController.navigate("reader/$encodedBookId")
                     }
-                )
-            }
+                },
+                onManageCategoriesClick = {
+                    navController.navigate(Screens.ManageCategories.route)
+                },
+                bookDetailsViewModel = bookDetailsViewModel
+            )
         }
 
-        // In AppNavHost.kt
-
         composable(
-            route = "reader/{bookId}",
-            arguments = listOf(navArgument("bookId") { type = NavType.StringType })
+            route = "reader/{bookId}?href={href}",
+            arguments = listOf(
+                navArgument("bookId") { type = NavType.StringType },
+                navArgument("href") {
+                    type = NavType.StringType
+                    nullable = true // It's optional!
+                    defaultValue = null
+                }
+            )
         ) { backStackEntry ->
             val bookId = backStackEntry.arguments?.getString("bookId") ?: return@composable
+            val href = backStackEntry.arguments?.getString("href")
 
             ReaderScreen(
                 bookId = bookId,
+                initialChapterHref = href,
                 onBackClick = {
                     navController.popBackStack()
                 }
