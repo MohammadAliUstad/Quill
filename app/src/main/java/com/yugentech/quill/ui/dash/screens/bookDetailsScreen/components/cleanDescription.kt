827ca037c7e5200ad894be30dcaf5077a465f2d1
Mohammad Ali <mohammadaliustad@gmail.com>
2026-02-18 14:20:00 +0530

    Cleanup!

--- app/src/main/java/com/yugentech/quill/ui/dash/screens/bookDetailsScreen/components/cleanDescription.kt
+++ app/src/main/java/com/yugentech/quill/ui/dash/screens/bookDetailsScreen/components/cleanDescription.kt
@@ -1,4 +1,6 @@
 package com.yugentech.quill.ui.dash.screens.bookDetailsScreen.components
 
-// Helper Functions
-
+fun cleanDescription(description: String?): String {
+    if (description.isNullOrBlank()) return "No description available."
+    return description.replace(Regex("<a\\b[^>]*>|</a>"), "").trim()
+}
