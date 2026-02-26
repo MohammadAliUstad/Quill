dc4393be9aec3a96f248b1b04ccdbe3eae75359d
Mohammad Ali <mohammadaliustad@gmail.com>
2026-02-26 14:20:00 +0530

    Updating Category Setup

--- app/src/main/java/com/yugentech/quill/category/CategoryModel.kt
+++ app/src/main/java/com/yugentech/quill/category/CategoryModel.kt
@@ -1,7 +1,18 @@
 package com.yugentech.quill.category
 
-data class Category(
+import com.yugentech.quill.room.entities.CategoryEntity
+
+data class CategoryModel(
     val id: Long,
     val name: String,
     val sortOrder: Int
 )
+
+fun CategoryModel.toEntity(isSystem: Boolean = false): CategoryEntity {
+    return CategoryEntity(
+        id = this.id,
+        name = this.name,
+        sortOrder = this.sortOrder,
+        isSystem = isSystem
+    )
+}
