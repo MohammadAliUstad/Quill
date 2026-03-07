b4f775bb9fb7a923a74f6c7b542e5e5f71558336
Mohammad Ali <mohammadaliustad@gmail.com>
2026-03-07 14:07:00 +0530

    Deleting unused files.

--- app/src/main/java/com/yugentech/quill/ui/dash/screens/bookDetailsScreen/components/cleanDescription.kt
+++
@@ -1,6 +0,0 @@
-package com.yugentech.quill.ui.dash.screens.bookDetailsScreen.components
-
-fun cleanDescription(description: String?): String {
-    if (description.isNullOrBlank()) return "No description available."
-    return description.replace(Regex("<a\\b[^>]*>|</a>"), "").trim()
-}
