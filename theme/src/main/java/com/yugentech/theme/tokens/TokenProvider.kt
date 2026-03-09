efca55cb23046fe825349f70f7de03e2a9caadb1
Mohammad Ali <mohammadaliustad@gmail.com>
2026-03-09 09:12:55 +0530

    Enhance Theme

--- theme/src/main/java/com/yugentech/theme/tokens/TokenProvider.kt
+++ theme/src/main/java/com/yugentech/theme/tokens/TokenProvider.kt
@@ -1,15 +1,15 @@
-package com.yugentech.quill.theme.tokens
+package com.yugentech.theme.tokens
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
-import com.yugentech.quill.theme.tokens.dimensions.ComponentTokens
-import com.yugentech.quill.theme.tokens.dimensions.CornerTokens
-import com.yugentech.quill.theme.tokens.dimensions.ElevationTokens
-import com.yugentech.quill.theme.tokens.dimensions.IconSizeTokens
-import com.yugentech.quill.theme.tokens.dimensions.SpacingTokens
-import com.yugentech.quill.theme.tokens.dimensions.StrokeTokens
+import com.yugentech.theme.tokens.dimensions.ComponentTokens
+import com.yugentech.theme.tokens.dimensions.CornerTokens
+import com.yugentech.theme.tokens.dimensions.ElevationTokens
+import com.yugentech.theme.tokens.dimensions.IconSizeTokens
+import com.yugentech.theme.tokens.dimensions.SpacingTokens
+import com.yugentech.theme.tokens.dimensions.StrokeTokens
 
 // Creates a CompositionLocal to provide tokens down the UI tree, defaulting to Compact
 val LocalDesignTokens = staticCompositionLocalOf { TokensCompact }
