e6666e49904eb03554baae2cc961d85c7a600813
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2026-05-05 11:15:22 +0530

    Quota management improved

--- app/src/main/java/com/yugentech/quill/quota/model/QuotaFields.kt
+++ app/src/main/java/com/yugentech/quill/quota/model/QuotaFields.kt
@@ -4,5 +4,4 @@ object QuotaFields {
     const val QUERIES_USED = "queriesUsed"
     const val QUERIES_LIMIT = "queriesLimit"
     const val RESET_AT = "resetAt"
-    const val IS_LIFETIME = "isLifetime"
 }
