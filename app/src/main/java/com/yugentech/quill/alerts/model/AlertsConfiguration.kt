ce2299df76f976fe5e75397b43ae05d671b018f8
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2026-05-21 11:15:22 +0530

    Clean Up!

--- app/src/main/java/com/yugentech/quill/alerts/model/AlertsConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package com.yugentech.sessions.alerts.model
-
-// Data model holding user preferences for sound, haptics, and background ambience
-data class AlertsConfiguration(
-    val soundEnabled: Boolean = true,
-    val hapticsEnabled: Boolean = true,
-    val backgroundSound: BackgroundSound = BackgroundSound.NONE
-)
