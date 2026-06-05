76e41440baf4b31650ebe2d3af052a32a2ea84c9
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2026-06-05 11:15:22 +0530

    Aira Setup Improvements

--- app/src/main/java/com/yugentech/quill/di/modules/shared/AiraModule.kt
+++ app/src/main/java/com/yugentech/quill/di/modules/shared/AiraModule.kt
@@ -1,17 +1,20 @@
 package com.yugentech.quill.di.modules.shared
 
-import com.yugentech.quill.aira.aira.repository.AiraChatRepository
-import com.yugentech.quill.aira.aira.repository.AiraChatRepositoryImpl
-import com.yugentech.quill.aira.aira.viewmodel.AiraViewModel
-import com.yugentech.quill.aira.book.BookRepository
-import com.yugentech.quill.aira.book.BookRepositoryImpl
+import com.yugentech.quill.aira.aira.util.AiraHandler
+import com.yugentech.quill.aira.bookChat.handler.BookChatHandler
+import com.yugentech.quill.aira.bookChat.service.BookChatService
+import com.yugentech.quill.aira.generalChat.handler.GeneralChatHandler
+import com.yugentech.quill.aira.generalChat.service.GeneralChatService
+import com.yugentech.quill.aira.intentDetection.repository.IntentDetectionRepository
+import com.yugentech.quill.aira.intentDetection.service.IntentDetectionService
 import com.yugentech.quill.aira.quick.repository.QuickRepository
 import com.yugentech.quill.aira.quick.repository.QuickRepositoryImpl
-import com.yugentech.quill.aira.quick.viewmodel.QuickViewModel
+import com.yugentech.quill.aira.quick.service.QuickActionService
 import com.yugentech.quill.aira.rag.EmbeddingEngine
 import com.yugentech.quill.aira.rag.RagRetriever
+import com.yugentech.quill.aira.repository.AiraChatRepository
+import com.yugentech.quill.aira.repository.AiraChatRepositoryImpl
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val airaModule = module {
@@ -30,53 +33,42 @@ val airaModule = module {
         )
     }
 
-    single<BookRepository> {
-        BookRepositoryImpl(
-            bookDao = get(),
-            chunkDao = get(),
-            indexingStateDao = get(),
-            workManager = get()
-        )
-    }
+    // --- Intent Detection ---
+    single { IntentDetectionService(functions = get()) }
+    single { IntentDetectionRepository(detectionService = get()) }
 
-    single<AiraChatRepository> {
-        AiraChatRepositoryImpl(
-            functions = get(),
-            ragRetriever = get(),
+    // --- Chat Flows ---
+    single { GeneralChatService(functions = get()) }
+    single { GeneralChatHandler(chatService = get()) }
+    single { BookChatService(functions = get()) }
+    single { BookChatHandler(chatService = get(), ragRetriever = get()) }
+
+    // --- Quick Action ---
+    single { QuickActionService(functions = get()) }
+
+    single {
+        AiraHandler(
+            intentDetectionRepository = get(),
+            generalChatHandler = get(),
+            bookChatHandler = get(),
             bookDao = get(),
             airaMessageDao = get()
         )
     }
 
-    viewModel { params ->
-        AiraViewModel(
-            bookId = params.get(),
-            airaChatRepository = get(),
-            bookRepository = get(),
-            quotaRepository = get(),
-            authRepository = get(),
-            workManager = get(),
-            billingRepository = get(),
+    single<AiraChatRepository> {
+        AiraChatRepositoryImpl(
+            airaHandler = get(),
+            airaMessageDao = get()
         )
     }
 
     single<QuickRepository> {
         QuickRepositoryImpl(
+            quickActionService = get(),
             ragRetriever = get(),
             bookChunkDao = get(),
-            bookDao = get(),
-            functions = get()
-        )
-    }
-
-    viewModel {
-        QuickViewModel(
-            airaChatRepository = get(),
-            quickRepository = get(),
-            quotaRepository = get(),
-            authRepository = get(),
-            bookRepository = get(),
-            billingRepository = get()
+            bookDao = get()
         )
     }
 }
