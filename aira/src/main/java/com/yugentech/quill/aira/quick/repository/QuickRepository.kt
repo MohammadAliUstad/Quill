de9033128dd97e7bf2097877e3836e8ef881b485
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2026-06-20 10:00:00 +0000

    Remove QuickChat legacy handler, repository, service, state, and viewmodel

--- aira/src/main/java/com/yugentech/quill/aira/quick/repository/QuickRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.yugentech.quill.aira.quick.repository
-
-import com.yugentech.quill.aira.quick.prompt.QuickPrompt
-import com.yugentech.quill.aira.response.AiraResponse
-import kotlinx.coroutines.flow.Flow
-
-interface QuickRepository {
-    suspend fun ask(bookId: String, quickPrompt: QuickPrompt): Flow<AiraResponse>
-}
