fd4afbb600ea87b0be47cb91291b0bbd5fe56e4f
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2026-06-22 10:45:00 +0530

    Update Room converters and app database schema

--- database/src/main/java/com/yugentech/quill/database/converter/RoomConverters.kt
+++ database/src/main/java/com/yugentech/quill/database/converter/RoomConverters.kt
@@ -4,6 +4,7 @@ import androidx.room.TypeConverter
 import com.yugentech.quill.database.model.BookSource
 import com.yugentech.quill.database.model.Chapter
 import com.yugentech.quill.database.model.DownloadStatus
+import com.yugentech.quill.database.model.HighlightStyle
 import com.yugentech.quill.database.model.RetrievedChunk
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -89,5 +90,15 @@ class RoomConverters {
         } catch (_: Exception) {
             emptyList()
         }
+    }
+
+    @TypeConverter
+    fun fromHighlightStyle(style: HighlightStyle): String = style.name
+
+    @TypeConverter
+    fun toHighlightStyle(value: String): HighlightStyle = try {
+        HighlightStyle.valueOf(value)
+    } catch (_: Exception) {
+        HighlightStyle.HIGHLIGHT
     }
 }
--- database/src/main/java/com/yugentech/quill/database/database/AppDatabase.kt
+++ database/src/main/java/com/yugentech/quill/database/database/AppDatabase.kt
@@ -47,7 +47,7 @@ import com.yugentech.quill.database.entity.HighlightEntity
     views = [
         LibraryBookView::class
     ],
-    version = 4,
+    version = 6,
     exportSchema = false
 )
 @TypeConverters(RoomConverters::class)
