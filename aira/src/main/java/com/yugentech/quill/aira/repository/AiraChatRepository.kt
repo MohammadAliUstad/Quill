8a4c74504fbf99149210af960c93444ac2f56850
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2026-06-30 09:00:00 +0000

    Update AiraChatRepository and impl to align with new service layer

--- aira/src/main/java/com/yugentech/quill/aira/repository/AiraChatRepository.kt
+++ aira/src/main/java/com/yugentech/quill/aira/repository/AiraChatRepository.kt
@@ -1,11 +1,11 @@
 package com.yugentech.quill.aira.repository
 
-import com.yugentech.quill.aira.aira.message.AiraMessage
+import com.yugentech.quill.aira.message.AiraMessage
 import com.yugentech.quill.aira.response.AiraResponse
 import kotlinx.coroutines.flow.Flow
 
 interface AiraChatRepository {
-    suspend fun ask(bookId: String, question: String): Flow<AiraResponse>
+    suspend fun ask(bookId: String, query: String): Flow<AiraResponse>
     suspend fun getMessagesForBook(bookId: String): Flow<List<AiraMessage>>
     suspend fun clearMessagesForBook(bookId: String)
 }
--- aira/src/main/java/com/yugentech/quill/aira/repository/AiraChatRepositoryImpl.kt
+++ aira/src/main/java/com/yugentech/quill/aira/repository/AiraChatRepositoryImpl.kt
@@ -1,20 +1,20 @@
 package com.yugentech.quill.aira.repository
 
-import com.yugentech.quill.aira.aira.message.AiraMessage
-import com.yugentech.quill.aira.aira.util.AiraHandler
+import com.yugentech.quill.aira.message.AiraMessage
 import com.yugentech.quill.aira.response.AiraResponse
+import com.yugentech.quill.aira.service.AiraChatService
 import com.yugentech.quill.database.dao.AiraMessageDao
 import com.yugentech.quill.database.entity.AiraMessageRole
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class AiraChatRepositoryImpl(
-    private val airaHandler: AiraHandler,
+    private val airaService: AiraChatService,
     private val airaMessageDao: AiraMessageDao
 ) : AiraChatRepository {
 
-    override suspend fun ask(bookId: String, question: String): Flow<AiraResponse> =
-        airaHandler.ask(bookId, question)
+    override suspend fun ask(bookId: String, query: String): Flow<AiraResponse> =
+        airaService.ask(bookId, query)
 
     override suspend fun getMessagesForBook(bookId: String): Flow<List<AiraMessage>> =
         airaMessageDao.getMessagesForBook(bookId).map { entities ->
