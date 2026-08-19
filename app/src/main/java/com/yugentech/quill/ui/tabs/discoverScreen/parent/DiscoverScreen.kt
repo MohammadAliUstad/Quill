466042d9e654c06e35541496a688b4b2e29eb29b
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2026-08-19 09:34:03 +0530

    Polishing

--- app/src/main/java/com/yugentech/quill/ui/tabs/discoverScreen/parent/DiscoverScreen.kt
+++ app/src/main/java/com/yugentech/quill/ui/tabs/discoverScreen/parent/DiscoverScreen.kt
@@ -8,11 +8,16 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.isImeVisible
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.widthIn
@@ -308,7 +313,6 @@ fun DiscoverScreen(
                         focusManager.clearFocus()
                     }
                 },
-                // 2. REMOVE .clip() and .border() from this outer modifier
                 modifier = Modifier.widthIn(min = dockedWidth),
                 colors = SearchBarDefaults.colors(
                     containerColor = animatedContainerColor,
